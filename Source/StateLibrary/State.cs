14b8203426ea2acc6024f588f9e0aadebef680db
Eric De Carufel <decarufe@users.noreply.github.com>
2013-03-15 07:15:49 -0400

    Rename solution add State pattern

---
+++ Source/StateLibrary/State.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateLibrary
+{
+  public abstract class State
+  {
+    public StateManager StateManager { get; set; }
+
+    public virtual void OnTerminate()
+    {
+
+    }
+
+    public virtual void OnInitialize(object parameter)
+    {
+
+    }
+
+    protected void ChangeState<T>(object parameter = null) where T : State, new()
+    {
+      StateManager.ChangeState<T>(parameter);
+    }
+  }
+}
---
+++ Source/StateLibrary/StateManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateLibrary
+{
+  public class StateManager
+  {
+    private State mCurrentState;
+
+    public State ChangeState<T>(object parameter = null) where T : State, new()
+    {
+      Terminate();
+
+      mCurrentState = Activator.CreateInstance<T>();
+      mCurrentState.StateManager = this;
+      mCurrentState.OnInitialize(parameter);
+
+      return null;
+    }
+
+    public void Terminate()
+    {
+      if (mCurrentState != null)
+      {
+        mCurrentState.OnTerminate();
+        mCurrentState = null;
+      }
+    }
+
+    public bool CurrentStateIs<T>()
+    {
+      return mCurrentState is T;
+    }
+  }
+}
