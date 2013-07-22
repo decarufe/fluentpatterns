a45b9f7953d1a7a452b3c510d412666e4ea0495d
Eric De Carufel <decarufe@users.noreply.github.com>
2013-07-22 13:35:09 -0400

    Refactor

--- Source/ChainOfResponsability.ConsoleApp/Program.cs
+++ Source/ChainOfResponsability.ConsoleApp/Program.cs
@@ -7,12 +7,16 @@ namespace ChainOfResponsability.ConsoleApp
   {
     private static void Main()
     {
-      ChainCommand chain = new ChainBuilder<ChainCommand>()
+      var chain = new ChainBuilder<ChainCommand>()
         .Add<Command1>()
         .Add<Command2>(() => new Command2("Test"))
         .Build();
-
       chain.Execute(null);
+      
+      var command1 = new Command1();
+      var command2 = new Command2("Test");
+      command1.NextInChain = command2;
+      command1.Execute(null);
 
       Console.WriteLine("Press enter to quit.");
       Console.ReadLine();
